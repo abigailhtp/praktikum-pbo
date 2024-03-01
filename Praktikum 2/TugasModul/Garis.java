beb8978e1906ee95589ddf1b95231f2b217d3d15
abigail <128231955+abigailhtp@users.noreply.github.com>
2024-03-01 13:18:08 +0700

    Add files via upload

--- Praktikum 2/TugasModul/Garis.java
+++ Praktikum 2/TugasModul/Garis.java
@@ -3,27 +3,53 @@
 //Hari/Tanggal: Rabu, 28 Februari 2024
 //Deskripsi: Konstruktor File kelas garis
 
-
 public class Garis {
-        private Titik t1,t2;
-        public Garis(Titik t1, Titik t2){
-            this.t1=t1;
-            this.t2=t2;
-        }
+    private Titik t1,t2;
+    public Garis(Titik t1, Titik t2){
+        this.t1=t1;
+        this.t2=t2;
+    }
 
-        public Titik getTitikAwal(){
-            return this.t1;
-        }
+    public Titik getTitikAwal(){
+        return this.t1;
+    }
 
-        public Titik getTitikAkhir(){
-            return this.t2;
-        }
+    public Titik getTitikAkhir(){
+        return this.t2;
+    }
 
-        public void setTitikAwal(Titik t1){
-            this.t1=t1;
-        }
+    public void setTitikAwal(Titik t1){
+        this.t1=t1;
+    }
 
-        public void setTitikAkhir(Titik t2){
-            this.t2=t2;
+    public void setTitikAkhir(Titik t2){
+        this.t2=t2;
+    }
+
+    public double getPanjang(){
+        return Math.pow((Math.pow((t2.getAbsis()-t1.getAbsis()), 2) + Math.pow((t2.getOrdinat()-t1.getOrdinat()), 2)), 0.5);
+    }
+
+    public double getGradien(){
+        return (t2.getOrdinat()-t1.getOrdinat())/(t2.getAbsis()-t1.getAbsis());
+    }
+
+    public void getRefleksiY(){
+        double absist1= t1.getAbsis();
+        double absist2= t2.getAbsis();
+        absist1*=(-1);
+        absist2*=(-1);
+        t1.setAbsis(absist1);
+        t2.setAbsis(absist2);
+    }
+
+    public boolean isTegakLurus(Garis G){
+        Garis A=new Garis (t1,t2);
+        if( G.getGradien()==A.getGradien()){
+            return true;
+        }else{
+            return false;
         }
+    }
 }
+
--- Praktikum 2/TugasModul/MGaris.java
+++ Praktikum 2/TugasModul/MGaris.java
@@ -5,15 +5,29 @@
 
 class MGaris {
     public static void main(String[] args){
-        Titik t1=new Titik(2,3);
-        Titik t2=new Titik(3,4);
+        Titik t1=new Titik(1,2);
+        Titik t2=new Titik(3,6);
+        Titik t3=new Titik(0,1);
+        Titik t4=new Titik(0,2);
 
         Garis G=new Garis(t1,t2);
+        Garis A=new Garis(t3,t4);
+        System.out.println("Titik awal:("+G.getTitikAwal().getAbsis()+","+G.getTitikAwal().getOrdinat()+")");
+        System.out.println("Titik akhir:("+G.getTitikAkhir().getAbsis()+","+G.getTitikAkhir().getOrdinat()+")");
+        System.out.println("Panjang garis:"+G.getPanjang());
+        System.out.println("Gradien garis:"+G.getGradien());
+
+        G.getRefleksiY();
+        System.out.println("Garis setelah direfleksi:");
         System.out.println("Titik awal:("+G.getTitikAwal().getAbsis()+","+G.getTitikAwal().getOrdinat()+")");
         System.out.println("Titik akhir:("+G.getTitikAkhir().getAbsis()+","+G.getTitikAkhir().getOrdinat()+")");
 
-
-
+        System.out. println("Apakah garis G dan garis A merupakan garis yang sejajar?");
+        if (G.isTegakLurus(A)){
+            System.out.println("Ya");
+        }else{
+            System.out.println("Tidak");
+        }
     }
 }
 //kesimpulan
@@ -23,4 +37,4 @@ class MGaris {
 //terdapat 4 relasi antarobjek:inheritance
 //asosiasi (has a) cth : handphone has a button
 //komposisi bagian yg gabisa dipisahkan cth laptop dengan cpu
-//agregasi bisa cth: laptop dengan mouse 
+//agregasi bisa cth: laptop dengan mouse
