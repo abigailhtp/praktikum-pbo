0389d49b71f7d4177912a03674356b9bc301e715
abigail <128231955+abigailhtp@users.noreply.github.com>
2024-03-09 17:22:37 +0700

    Add files via upload

---
+++ Praktikum 3/TugasTambahan/PeminjamanBuku/src/Anggota.java
@@ -0,0 +1,37 @@
+public class Anggota {
+        private String nama;
+        private Buku[] bukuPinjaman;
+        private int jumlahBukuPinjaman;
+
+        public Anggota(String nama) {
+            this.nama = nama;
+            bukuPinjaman = new Buku[3];
+            jumlahBukuPinjaman = 0;
+        }
+
+        public String getNama() {
+            return nama;
+        }
+
+        public Buku[] getBukuPinjaman() {
+            return bukuPinjaman;
+        }
+
+        public int getJumlahBukuPinjaman() {
+            return jumlahBukuPinjaman;
+        }
+
+        public void pinjamBuku(Buku buku) throws MaksimumBukuTerpinjamException,BukuTidakTersediaException{
+            // TODO 2: Buat kondisi untuk mencegah jumlah buku yang dipinjam melebihi 3 dengan throw keyword
+            if(jumlahBukuPinjaman<3){
+                throw new MaksimumBukuTerpinjamException("Anggota sudah melebihi batas meminjam buku");
+                // TODO 3: Buat kondisi untuk mencegah buku yang tidak tersedia dipinjam dengan throw keyword
+            }else if(!buku.isTersedia()){
+                throw new BukuTidakTersediaException();
+            }
+            buku.setTersedia(false);
+            bukuPinjaman[jumlahBukuPinjaman] = buku;
+            jumlahBukuPinjaman++;
+        }
+
+}
---
+++ Praktikum 3/TugasTambahan/PeminjamanBuku/src/BukuTidakTersediaException.java
@@ -0,0 +1,5 @@
+public class BukuTidakTersediaException extends Exception{
+    public BukuTidakTersediaException() {
+        super("Buku tidak tersedia");
+    }
+}
