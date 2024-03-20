1c70ca0cf0c18b221e750d346a9e5934f7ed08ef
abigail <128231955+abigailhtp@users.noreply.github.com>
2024-03-20 08:37:27 +0700

    Add files via upload

---
+++ Praktikum 5/TugasModul/src/MLingkaran.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class MLingkaran {
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Masukkan jari-jari lingkaran :");
+        double jejari=scan.nextDouble();
+        Lingkaran l=new Lingkaran(jejari);
+        System.out.println("Luas lingkaran dengan jejari "+jejari+" satuan adalah "+l.hitungLuas());
+    }
+}
